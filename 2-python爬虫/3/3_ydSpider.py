33fde78b81f8fe5ad590a695985bfb638b59b3ff
KongFuCrawl <lneng8728@gmail.com>
2025-10-21 05:34:37 +0800

    描述新功能

---
+++ 2-python爬虫/3/3_ydSpider.py
@@ -0,0 +1,43 @@
+
+
+headers = {
+    "Host": "dict.youdao.com",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Length": "330",
+    "Origin": "https://fanyi.youdao.com",
+    "Connection": "keep-alive",
+    "Referer": "https://fanyi.youdao.com/",
+    "Cookie": "OUTFOX_SEARCH_USER_ID=1204419532@123.13.67.120; OUTFOX_SEARCH_USER_ID_NCOO=1812469373.2692113; YOUDAO_MOBILE_ACCESS_TYPE=1; __adroll_fpc=b3a3bb7e55a2ce65fb7b7686e3725eba-1760252400700; _uetsid=0db1c7c0a73811f0be61f3a3f0f14684; _uetvid=0db1b140a73811f0941a8fcdfcea7a9f; DICT_DOCTRANS_SESSION_ID=NDYwYTg2MWYtODE2ZS00ZGIyLTgxNzItYTkwMTM2ZmMxOWU5",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+}
+
+
+date = {
+    "i": "\xe5\xa4\x9c",
+    "from": "zh-CHS",
+    "to": "en",
+    "useTerm": "'false'",
+    "domain": "'0'",
+    "dictResult": "'true'",
+    "keyid": "webfanyi",
+    "sign": "6f94e801966e0d38cfc69c8f78e3152e",
+    "client": "fanyideskweb",
+    "product": "webfanyi",
+    "appVersion": "1.0.0",
+    "vendor": "web",
+    "pointParam": "client,mysticTime,product",
+    "mysticTime": "'1760344108935'",
+    "keyfrom": "fanyi.web",
+    "mid": "'1'",
+    "screen": "'1'",
+    "model": "'1'",
+    "network": "wifi",
+    "abtest": "'0'",
+    "yduuid": "abcdefg",
+}
---
+++ 2-python爬虫/4/1_selenium.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from time import sleep
+webdriver = webdriver.Chrome()
+time = url = 'https://www.baidu.com/'
+webdriver.get(time)
+time(sleep(5))
